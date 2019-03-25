ced962b61586218beeb2a00855643ccf6b5cb817
Guðmundur Helgi Rósuson <GHR@Gumundurs-MacBook-Pro.local>
2019-03-25 13:58:15 +0000

    viðbót frá Gumma

--- main.js
+++ main.js
@@ -3,6 +3,19 @@ window.addEventListener("load", () => {
   fetch("http://localhost:3000/api")
     .then(r => r.json())
     .then(data => {
-      document.getElementById("");
+      // document.getElementById("main").innerHTML += `
+      //   <div>${data[0].name}</div>
+      //   <img src="${data[0].pic}"/>
+      // `
+      displayImages(data);
+      console.log(data)
     });
 });
+displayImages = (data) =>{
+  data.forEach(image =>{
+    document.getElementById("main").innerHTML += `
+        <div>${image.name}</div>
+        <img src="${image.pic}"/>
+      `  
+  })
+}
